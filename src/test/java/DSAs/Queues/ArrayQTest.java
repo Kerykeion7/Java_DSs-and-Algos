1b20667a09646f9f8432ee6c8d4eb1843ee523e4
Jens D <jensdeweirdt@outlook.be>
2020-11-24 11:19:25 +0100

    Add ArrayQ tests

--- src/test/java/DSAs/Queues/ArrayQTest.java
+++ src/test/java/DSAs/Queues/ArrayQTest.java
@@ -12,4 +12,43 @@ public class ArrayQTest {
         Queue<String> q = new Queue<>();
         assertNotNull(q);
     }
+
+    @Test
+    void testSize(){
+        Queue<String> q = new Queue<>();
+        assertTrue(q.isEmpty() 
+        && !q.isFull()
+        && q.getSize() == 0);
+
+        q.enqueue("one");
+        q.enqueue("two");
+        q.enqueue("three");
+
+        assertTrue(!q.isEmpty() 
+        && q.getSize() == 3);
+
+        q.enqueue("four");
+        q.enqueue("five");
+        q.enqueue("six");
+
+        assertTrue(q.isFull());
+    }
+
+    @Test
+    void testDequeuing(){
+        Queue<Integer> q = new Queue<>();
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        q.enqueue(4);
+        q.enqueue(5);
+        q.enqueue(6);
+        assertTrue(q.getSize() == 6);
+        q.enqueue(7);
+        assertFalse(q.getSize() == 7);
+        assertTrue(q.dequeue() == 1 &&
+        q.dequeue() == 2
+        && q.getSize() == 4);
+
+    }
 }
